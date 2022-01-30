7405d7da4748e9e7ba80cdccf501726f46cfa07c
dmarjanovic2724 <dmarjanovic2724@gmail.com>
2022-01-30 22:02:54 +0100

    change doc title

--- commponents/head.php
+++ commponents/head.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="style.css">   
     <script src="https://kit.fontawesome.com/a78c659ce6.js" crossorigin="anonymous"></script>    
-    <title>Document</title>
+    <title>Forum</title>
 </head>
 <body>
     <?php include "nav.php"; ?>
